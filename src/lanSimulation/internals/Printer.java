a4679eb784d342d3d333818e92b12a6a430b8f95
vpe0001 <vpe0001@alu.ubu.es>
2015-06-04 20:52:14 +0200

    #5 Crea subclases vacías para los diferentes tipos de nodo ("WorkStation", "Printer").

---
+++ src/lanSimulation/internals/Printer.java
@@ -0,0 +1,14 @@
+package lanSimulation.internals;
+
+public class Printer extends Node{
+	public Printer(byte type, String name) {
+		super(type, name);
+		// TODO Auto-generated constructor stub
+	}
+	
+	public Printer(byte type, String name, Node nextNode) {
+		super(type, name, nextNode);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/lanSimulation/internals/WorkStation.java
@@ -0,0 +1,15 @@
+package lanSimulation.internals;
+
+public class WorkStation extends Node {
+
+	public WorkStation(byte type, String name) {
+		super(type, name);
+		// TODO Auto-generated constructor stub
+	}
+	
+	public WorkStation(byte type, String name, Node nextNode) {
+		super(type, name, nextNode);
+		// TODO Auto-generated constructor stub
+	}
+
+}
